442fec20d6383a83d7dde8bc463a5feedddd64f4
mastragelistasos@gmail.com <mastragelistasos@gmail.com>
2024-01-08 20:36:21 +0200

    added components, changed event.actions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,16 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Collection from "@/components/shared/Collection";
+import { getAllEvents } from "@/lib/actions/event.actions";
 
-export default function Home() {
+export default async function Home() {
+  const events = await getAllEvents({
+    query: "",
+    category: "",
+    page: 1,
+    limit: 6,
+  });
+
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -42,7 +50,15 @@ export default function Home() {
           Search CategoryFilter
         </div>
 
-        <Collection />
+        <Collection
+          data={events?.data}
+          emptyTitle="No Events Found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
       </section>
     </>
   );
