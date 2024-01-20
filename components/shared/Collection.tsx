6ed26a47408432f1ddbc5c9ae67d7e81dafb5f24
mastragelistasos@gmail.com <mastragelistasos@gmail.com>
2024-01-20 16:07:06 +0200

    fixed error on urlParamName not being optional in collectionProps

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -9,7 +9,7 @@ type CollectionProps = {
   limit: number;
   page: number | string;
   totalPages?: number;
-  urlParamName: string;
+  urlParamName?: string;
   collectionType?: "Events_Organized" | "My_Tickets" | "All_Events";
 };
 
