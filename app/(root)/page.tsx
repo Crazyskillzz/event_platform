c40fb819f1e652603f77004daa85a986f88283d6
mastragelistasos@gmail.com <mastragelistasos@gmail.com>
2024-01-21 11:39:58 +0200

    implementing search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Collection from "@/components/shared/Collection";
 import { getAllEvents } from "@/lib/actions/event.actions";
+import Search from "@/components/shared/Search";
 
 export default async function Home() {
   const events = await getAllEvents({
@@ -47,7 +48,8 @@ export default async function Home() {
           Trusted by <br /> Thousands of Events
         </h2>
         <div className="flex w-full flex-col gap-5 md:flex-row">
-          Search CategoryFilter
+          <Search />
+          CategoryFilter
         </div>
 
         <Collection
